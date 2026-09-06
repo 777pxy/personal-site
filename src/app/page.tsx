a78bafa733540497248d60710e0593bdf96f9e5a
pxy05 <psky0695@gmail.com>
2026-09-06 22:36:34 +0100

    Added conditional rendering on based on screen size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { BigText } from "@/components/common/BigText";
-import { LinedShowcase } from "@/components/common/LinedShowcase";
 import { LinedShowcaseHeaderData } from "@/components/common/LinedShowcaseHeader";
-import { Row } from "@/components/common/Row";
-import { TextBanner } from "@/components/common/TextBannner";
-import Image from "next/image";
-import placeholder from "@/assets/personal-site-bg-dark.jpg";
-import bathGorgon from "@/assets/Bath_Gorgon.webp";
+import DesktopFrontPage from "@/components/layouts/desktop/DesktopFrontPage";
+import MobileFrontPage from "@/components/layouts/mobile/MobileFrontPage";
 
 const sections: LinedShowcaseHeaderData[] = [
   {
@@ -24,38 +19,13 @@ const sections: LinedShowcaseHeaderData[] = [
 
 export default function Page() {
   return (
-    <div className="pt-8 flex flex-col">
-      <Row
-        left={
-          <>
-            <BigText text="Full Stack" />
-            <span className="pl-2 text-xl">SOFTWARE ENGINEER</span>
-          </>
-        }
-        right={<BigText text="Praj" />}
-      />
-      <Row
-        className="-mt-3 -mb-2"
-        left={
-          <>
-            <BigText text="Bath University" />
-          </>
-        }
-        right={<BigText text="Yakha" />}
-      />
-      <TextBanner
-        className="-z-1 py-3"
-        left={<BigText text="Steak Specialist." />}
-        right={<Image src={bathGorgon} alt="bath gorgon" className="w-25" />}
-      />
-      <LinedShowcase
-        image={
-          <div className="min-h-[70svh] mt-4 mr-4">
-            <Image src={placeholder} alt="hi guys!" width={1920} height={40} />
-          </div>
-        }
-        items={sections}
-      />
-    </div>
+    <>
+      <div className="hidden md:block">
+        <DesktopFrontPage />
+      </div>
+      <div className="block md:hidden">
+        <MobileFrontPage />
+      </div>
+    </>
   );
 }
---
+++ src/components/layouts/mobile/MobileFrontPage.tsx
@@ -0,0 +1,61 @@
+import { BigText } from "@/components/common/BigText";
+import { LinedShowcase } from "@/components/common/LinedShowcase";
+import { LinedShowcaseHeaderData } from "@/components/common/LinedShowcaseHeader";
+import { Row } from "@/components/common/Row";
+import { TextBanner } from "@/components/common/TextBannner";
+import Image from "next/image";
+import placeholder from "@/assets/personal-site-bg-dark.jpg";
+import bathGorgon from "@/assets/Bath_Gorgon.webp";
+
+const sections: LinedShowcaseHeaderData[] = [
+  {
+    title: "Home Lab",
+    tags: ["Linux", "Ubuntu", "Docker", "Networking", "Nginx"],
+  },
+  {
+    title: "KJDetailz",
+    tags: ["Next.js", "Claude Code", "Vercel", "Github Actions"],
+  },
+  {
+    title: "OwnMi",
+    tags: ["Linux", "Ubuntu", "Docker", "Networking", "Nginx"],
+  },
+];
+
+export default function MobileFrontPage() {
+  return (
+    <div className="pt-8 flex flex-col">
+      <Row
+        left={
+          <>
+            <BigText text="Full Stack" />
+            <span className="pl-2 text-xl">SOFTWARE ENGINEER</span>
+          </>
+        }
+        right={<BigText text="Praj" />}
+      />
+      <Row
+        className="-mt-3 -mb-2"
+        left={
+          <>
+            <BigText text="Bath University" />
+          </>
+        }
+        right={<BigText text="Yakha" />}
+      />
+      <TextBanner
+        className="-z-1 py-3"
+        left={<BigText text="Steak Specialist." />}
+        right={<Image src={bathGorgon} alt="bath gorgon" className="w-25" />}
+      />
+      <LinedShowcase
+        image={
+          <div className="min-h-[70svh] mt-4 mr-4">
+            <Image src={placeholder} alt="hi guys!" width={1920} height={40} />
+          </div>
+        }
+        items={sections}
+      />
+    </div>
+  );
+}
